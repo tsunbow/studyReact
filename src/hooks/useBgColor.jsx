363dbd899b6122b1d6da224fe9b358f43bed75a7
tsuyamakazuki <kazuki_tsuyama@bita.jp>
2022-07-15 15:39:41 +0900

    ✨ useRouterの実装

---
+++ src/hooks/useBgColor.jsx
@@ -0,0 +1,29 @@
+import { useRouter } from "next/router";
+import {useEffect, useMemo} from "react";
+
+export const useBgColor = () => {
+  const router = useRouter();
+  console.log(router);
+
+  const bgColor = useMemo(() => {
+    switch(router.pathname) {
+      case "/": {
+        return "lightblue"
+      }
+      case "/about": {
+        return "beige"
+      }
+      default: {
+        return "";
+      }
+    }
+    // return router.pathname ===  "/" ? "lightblue" : "beige";
+  },[router.pathname])
+
+  useEffect(() => {
+    document.body.style.backgroundColor = bgColor;
+    return() => {
+      document.body.style.backgroundColor = ""
+    }
+  }, [bgColor]);
+}
